32b9e7eb9dd62878655fb139094e36859a5483b2
UfukAcar1 <fakfukcode@outlook.com>
2024-05-14 14:28:15 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ufuk Acar on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
